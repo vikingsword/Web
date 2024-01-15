e9442bd574d88c28341d63a1a7772857a7ef70f1
vikingar <423141759@qq.com>
2024-01-15 15:56:02 +0800

    vue2

---
+++ VUE/component-demo/src/components/Movie.vue
@@ -0,0 +1,22 @@
+<template>
+    <!-- vue2 中多个组件都要包含在一个根组件下， 不能有两个 div -->
+    <div>
+        <h1>{{title}}</h1>
+    </div>    
+</template>
+
+
+<script>
+    export default{
+        name: "movies",
+        data: function() {
+            return {
+                title: "breaking bad !"
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
