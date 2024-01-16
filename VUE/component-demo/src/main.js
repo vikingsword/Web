be565b48f07103d0ad5ae4cc97c3bb6971f82bee
vikingar <423141759@qq.com>
2024-01-16 16:55:27 +0800

    vue

--- VUE/component-demo/src/main.js
+++ VUE/component-demo/src/main.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-import 'font-awesome/css/font-awesome.min.css'
+import 'font-awesome/css/font-awesome.min.css';
+import axios from 'axios'
 
+axios.defaults.baseURL = "http://localhost:8081"
+// 将axios挂载到vue上面，给vue加了一个http的属性，属性值为axios
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
