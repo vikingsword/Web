af9fb803a4f548f293bfe881179462aa9f0f9085
vikingar <423141759@qq.com>
2024-01-20 19:18:08 +0800

    vuex

--- VUE/vuex-demo/src/components/HelloWorld.vue
+++ VUE/vuex-demo/src/components/HelloWorld.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="hello">
-   
+   {{ this.$store.state.count }}
+   <button @click="add">+1</button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
+  methods: {
+    add() {
+      // this.$store.state.count += 1
+      // 应该通过 mutation 修改
+      this.$store.commit("increment")
+    }
+  }
 }
 </script>
 
--- VUE/vuex-demo/src/store/index.js
+++ VUE/vuex-demo/src/store/index.js
@@ -14,3 +14,4 @@ const store = new Vuex.Store({
     }
 })
 
+export default store
